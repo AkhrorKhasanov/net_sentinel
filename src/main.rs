680bec1cb6143fcb970486927c32c37c19ae0c30
AkhrorKhasanov <ahrorhasanov815@gmail.com>
2026-06-21 20:35:21 +0500

    21.06.2026 20:35

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+mod models;
+mod logger;
+mod processor;
+mod sniffer;
+
+use clap::Parser;
+use tokio::sync::mpsc;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long)]
+    ip: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+    let (tx, rx) = mpsc::channel(100);
+
+    println!("net_sentinel monitoring {}...", args.ip);
+
+    sniffer::start_sniffer(args.ip, tx);
 }
