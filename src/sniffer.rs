890e01b890503d036b272b0ffdf1d4f7017a9f5d
AkhrorKhasanov <ahrorhasanov815@gmail.com>
2026-06-21 22:50:03 +0500

    21.06.2026 22:50

--- src/sniffer.rs
+++ src/sniffer.rs
@@ -1,18 +1,36 @@
 use pnet::datalink::{self, Channel::Ethernet};
+use pnet::packet::Packet;
 use tokio::sync::mpsc::Sender;
 use crate::processor::process_packet;
 
 pub fn start_sniffer(target_ip: String, tx: Sender<crate::models::AuditEntry>) {
-    let interface = datalink::interfaces().into_iter().find(|i| i.is_up()).expect("Interface not found");
+    let interfaces = datalink::interfaces();
+    
+    let interface = interfaces
+        .into_iter()
+        .find(|i| i.name == "eth0")
+        .expect("eth0 interfeysi topilmadi! Iltimos, interfeys nomini tekshiring.");
+
+    println!("NetSentinel: {} interfeysi orqali tinglash boshlandi...", interface.name);
+
     let (_, mut rx) = match datalink::channel(&interface, Default::default()) {
-        Ok(Ethernet(tx, rx)) => (tx, rx), 
-        _ => panic!("Error opening channel"),
+        Ok(Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("Faqat Ethernet kanallari qo'llab-quvvatlanadi"),
+        Err(e) => panic!("Kanalni ochishda xatolik: {}", e),
     };
 
+    println!("NetSentinel: Paketlar kutilmoqda...");
+
     loop {
-        if let Ok(packet) = rx.next() {
-            if let Some(entry) = process_packet(packet, &target_ip) {
-                let _ = tx.blocking_send(entry);
+        match rx.next() {
+            Ok(packet) => {
+
+                if let Some(entry) = process_packet(packet, &target_ip) {
+                    let _ = tx.blocking_send(entry);
+                }
+            }
+            Err(e) => {
+                eprintln!("Xatolik: {}", e);
             }
         }
     }
