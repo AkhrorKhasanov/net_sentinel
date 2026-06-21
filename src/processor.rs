da036045fd61ba63c94a4fde6ae8861f30dec5e6
AkhrorKhasanov <ahrorhasanov815@gmail.com>
2026-06-22 01:39:46 +0500

    22.06.2026 01:39

--- src/processor.rs
+++ src/processor.rs
@@ -1,11 +1,18 @@
 use pnet::packet::ipv4::Ipv4Packet;
 use pnet::packet::tcp::TcpPacket;
+use pnet::packet::ethernet::{EthernetPacket, EtherTypes};
 use pnet::packet::Packet;
 use crate::models::AuditEntry;
 use chrono::Local;
 
 pub fn process_packet(packet: &[u8], target_ip: &str) -> Option<AuditEntry> {
-    let ipv4 = Ipv4Packet::new(&packet[14..])?;
+    let eth = EthernetPacket::new(packet)?;
+
+    if eth.get_ethertype() != EtherTypes::Ipv4 {
+        return None;
+    }
+
+    let ipv4 = Ipv4Packet::new(eth.payload())?;
     
     let src_ip = ipv4.get_source().to_string();
     let dst_ip = ipv4.get_destination().to_string();
@@ -15,8 +22,8 @@ pub fn process_packet(packet: &[u8], target_ip: &str) -> Option<AuditEntry> {
     }
 
     if let Some(tcp) = TcpPacket::new(ipv4.payload()) {
-        
         let dst_port = tcp.get_destination();
+        
         if dst_port != 80 && dst_port != 443 {
             return None;
         }
